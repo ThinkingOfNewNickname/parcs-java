1e599ab2b2e9c229bb659bebba77570421a3ac4f
Bohdan Filonenko <bodyaufml@gmail.com>
2023-06-28 11:24:30 +0200

    Debug iteration 8

--- src/Node.java
+++ src/Node.java
@@ -11,31 +11,31 @@ import java.io.Serializable;
 // }
 
 public class Node implements Serializable {
-    private int id;
-    // private int time;
+    // private int id;
+    private int time;
     // private List<Node> deps;
 
     public Node(int id) {
         this.id = id;
-        // this.time = 0;
+        this.time = 0;
         // this.deps = new ArrayList<>();
     }
 
-    public int getId() {
-        return id;
-    }
-
-    // public int getTime() {
-    //     return time;
+    // public int getId() {
+    //     return id;
     // }
+
+    public int getTime() {
+        return time;
+    }
 
     // public List<Node> getDeps() {
     //     return deps;
     // }
 
-    // public void setTime(int time) {
-    //     this.time = time;
-    // }
+    public void setTime(int time) {
+        this.time = time;
+    }
 
     // public void addDep(Node dep) {
     //     deps.add(dep);
